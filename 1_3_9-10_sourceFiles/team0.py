f3b850ff40ce9e07cead6b7c986759c9279a6656
Rocket26 <eli.r.wb.rc.cc@gmial.com>
2019-02-12 10:45:49 -0600

    Update team0.py

--- 1_3_9-10_sourceFiles/team0.py
+++ 1_3_9-10_sourceFiles/team0.py
@@ -1,68 +1,80 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+import inspect
+import prisoners_dilemma
+import os
+print('---',os.path.dirname(os.path.abspath(__file__)) )
+team_name = 'w' 
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
+    global bs
+    if len(my_history) == 0:
+        player1Id = ((inspect.getargvalues(inspect.currentframe().f_back))[3])['player1']
+        player2Id = ((inspect.getargvalues(inspect.currentframe().f_back))[3])['player2']       
+        myId =inspect.getfile(inspect.currentframe()) 
+        if str(myId) in str(player1Id):
+            myId = player1Id  
+            oppId = player2Id       
+        elif str(myId) in str(player2Id):
+            myId = player2Id
+            oppId = player1Id     
+        bs = (runSim(oppId,myId))   
+    move = bs.move(my_history, their_history, my_score, their_score)  
+    return move
+def runSim(o,me):   
+    modSs = []
+    mods = prisoners_dilemma.modules  
+    for i in range(0,(len( mods)-1)):
+        if me ==  mods[i]:
+            del  mods[i]
+    for i in range(0,(len( mods)-1)):
+        modSs.append([pG(( mods[i]),o),(( mods[i]).team_name),(o.team_name),( mods[i])])
+    modSs.sort(key = lambda x: x[0])
+    modSs = modSs[::-1]
+    return((modSs[0])[-1])
+def pG(m,o):
+    mS = 0
+    oS = 0
+    mH = []
+    oH = []
+    for round in range(200):
+        mS, oS, mH, oH = pR(m, o, mS, oS, mH, oH) 
+    return mS
+def pR(m,o,mS,oS,mH,oH):
+    R = 0 
+    T = 100 
+    SP = -500 
+    P = -250 
+    E = -250
+    a1 = m.move(mH,oH,mS,oS)
+    a2 = o.move(oH,mH,oS,mS) 
+    if (type(a1) != str) or (len(a1) != 1):
+        a1=' '   
+    if (type(a2) != str) or (len(a2) != 1):
+        a2=' '
+    a3 = a1 + a2   
+    if a3 == 'cc':
+        mS += R
+        oS += R    
+    elif a3 == 'cb':
+        mS += SP
+        oS += T   
+    elif a3 == 'bc':
+        mS += T
+        oS += SP     
+    elif a3 == 'bb':   
+        mS += P
+        oS += P      
     else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+        mS += E
+        oS += E  
+    if a1 in 'bc':
+        mH += a1   
+    else:
+        mH += ' '
+    if a2 in 'bc': 
+        oH += a2
+    else: 
+        oH += ' '   
+    return (mS,oS,mH,oH)  
+         
